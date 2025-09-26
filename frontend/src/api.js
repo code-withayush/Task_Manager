23ee74e8fb1d4da14718bf57a84c586c78accd34
code-withayush <ayushkumar55353@gamil.com>
2025-09-26 13:05:09 +0530

    Frontend ready for deploy

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -10,7 +10,7 @@
 // });
 
 // export default API;
- import axios from "axios";
+import axios from "axios";
 
 const API = axios.create({
   baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
